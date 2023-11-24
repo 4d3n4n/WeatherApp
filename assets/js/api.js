1c9046caf3329ccecbc9f9c36262a032ca39f0d3
4d3n4n <akhachnane@gmail>
2023-11-24 13:36:42 +0100

    first commit

---
+++ assets/js/api.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const api_key = "4e04660ba9b1bf5356ed8e744b5be610";
+
+/**
+ *   fetch data from server 
+ *   @param {string} URL API url
+ *   @param {function} callback callback
+*/
+
+export const fetchData = function(URL, callback) {
+    fetch(`${URL}&appid=${api_key}`)
+    .then(res => res.json())
+    .then(data => callback(data));
+}
+
+export const url = {
+    currentWeather(lat, lon) {
+        return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric`;
+    },
+    forecast(lat, lon) {
+        return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric`
+    },
+    airPollution(lat, lon) {
+        return `https://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`
+    },
+    reverseGeo(lat, lon) {
+        return `https://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`
+    },
+    /**
+     * @param {string} query Search query e.g.: "London", "New York",
+     */
+    geo(query) {
+        return `https://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`
+    }
+}
+
